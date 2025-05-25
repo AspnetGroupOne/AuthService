7721f91fb5a3d7a7736fce9d0cda7727fbbc0541
Fredrik <freddy_hammer@hotmail.se>
2025-05-25 18:03:24 +0200

    Added JsonProperty to see if it helps in SigninResult

--- AuthServiceProvider/Models/SignInResult.cs
+++ AuthServiceProvider/Models/SignInResult.cs
@@ -1,9 +1,14 @@
-﻿namespace AuthServiceProvider.Models
+﻿using Newtonsoft.Json;
+
+namespace AuthServiceProvider.Models
 {
     public class SignInResult
     {
+        [JsonProperty("success")]
         public bool Success { get; set; }
+        [JsonProperty("message")]
         public string? Message { get; set; }
+        [JsonProperty("userId")]
         public string? UserId { get; set; }
         public string? AccessToken { get; set; }
         public string? RefreshToken { get; set; }
